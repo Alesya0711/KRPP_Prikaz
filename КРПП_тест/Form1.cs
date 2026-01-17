692c6ad0f22d7e79b431d96eed44052ceda23caf
Alesya0711 <pisareva.alesya@gmail.com>
2026-01-17 10:19:25 +0300

    Определение полей для ввода

--- КРПП_тест/Form1.cs
+++ КРПП_тест/Form1.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace КРПП_тест
+{
+    public partial class Form1 : Form
+    {
+        private Label lblNumber, lblCity, lblDay, lblResponsible, lblDeputy, lblPerson1, lblPerson2, lblPerson3;
+        private TextBox txtNumber, txtCity, txtDay, txtResponsible, txtDeputy, txtPerson1, txtPerson2, txtPerson3;
+        private Button btnSave;
+
